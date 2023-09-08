08d157b687dc81fb3e8ada76077fd327de0b77e0
neha074 <nehamanickam74@gmail.com>
2023-09-08 22:43:32 +0530

    init

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+const port = 3000;
+
+app.get("/api/getinfo", async (req, res) => {
+  try {
+    const htmlResponse = await axios.get("https://time.com");
+
+    const $ = cheerio.load(htmlResponse.data);
+
+    const lateststories = $("div.partial.latest-stories > ul > li")
+      .map((index, element) => {
+        const headingText = $(element).text();
+        const link = $(element).find("a").attr("href");
+
+        return {
+          title: headingText,
+          link: "https://time.com" + link,
+
+        };
+      })
+      .get();
+
+    res.json(lateststories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
